6916086fb764a62b504ed891ad51130f65f95b8f
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-09-06 21:50:27 -0300

    adicionei alguns comentários

--- C208-B/aula4_parte2.asm
+++ C208-B/aula4_parte2.asm
@@ -1,9 +1,9 @@
 # definindo valores para os registradores
-li $t1, 0x10203040
-li $t2, 0x5060
-li $t3, 0x70
+li $t1, 0x10203040	# definindo um valor para o registrador $t1
+li $t2, 0x5060		# definindo um valor para o registrador $t2
+li $t3, 0x70		# definindo um valor para o registrador $t3
 
 # passando os valores para a memoria a partir do primeiro resgitradador
 sw $t1, 0($s0)	# sw -> store word: guarda 4 bits dos registradores na memoria
-sh $t2, 4($s0)	# sh -> store word: guarda 2 bits dos registradores na memoria
-sb $t3, 6($s0)	# sb -> store word: guarda 1 bits dos registradores na memoria
+sh $t2, 4($s0)	# sh -> store half: guarda 2 bits dos registradores na memoria
+sb $t3, 6($s0)	# sb -> store bit: guarda 1 bits dos registradores na memoria
