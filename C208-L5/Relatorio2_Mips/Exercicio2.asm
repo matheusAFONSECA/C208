df6b701ee88728287c7372d9d30fa17e92b69709
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-10-09 23:38:01 -0300

    Exercicio 2, 3 e 4 OK

---
+++ C208-L5/Relatorio2_Mips/Exercicio2.asm
@@ -0,0 +1,15 @@
+.text
+   # definindo os valores para os registradores
+   li $t1, 324		
+   li $t2, 100		
+   and $t3, $t1, $t2	# realiza a operação "and" entre os numeros em binario e manda para o resgistrador $t3 o resultado
+   
+   # definindo os valores para os registradores
+   li $t1, 2018		
+   li $t2, 1970		
+   and $t3, $t1, $t2	# realiza a operação "and" entre os numeros em binario e manda para o resgistrador $t3 o resultado
+   
+   # definindo os valores para os registradores
+   li $t1, 33		
+   li $t2, 66		
+   and $t3, $t1, $t2	# realiza a operação "and" entre os numeros em binario e manda para o resgistrador $t3 o resultado
---
+++ C208-L5/Relatorio2_Mips/Exercicio3.asm
@@ -0,0 +1,15 @@
+.text
+   # definindo os valores para os registradores
+   li $t1, 324		
+   li $t2, 100		
+   or $t3, $t1, $t2	# realiza a operação "or" entre os numeros em binario e manda para o resgistrador $t3 o resultado
+   
+   # definindo os valores para os registradores
+   li $t1, 2018		
+   li $t2, 1970		
+   or $t3, $t1, $t2	# realiza a operação "or" entre os numeros em binario e manda para o resgistrador $t3 o resultado
+   
+   # definindo os valores para os registradores
+   li $t1, 33		
+   li $t2, 66		
+   or $t3, $t1, $t2	# realiza a operação "or" entre os numeros em binario e manda para o resgistrador $t3 o resultado
